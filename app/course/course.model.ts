20a661b5ba01b274d1b1180ff525dea5c3ae685e
Chirag H <145862709+ChiragHariprasad@users.noreply.github.com>
2024-10-21 00:02:14 +0530

    Create course.model.ts

---
+++ app/course/course.model.ts
@@ -0,0 +1,9 @@
+export interface Course {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  videoUrl: string;
+  textContent: string;
+  interactiveContent: string;
+}
